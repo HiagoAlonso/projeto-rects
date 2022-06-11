7d06a4d494f8d426470344b7dae81f4e644518d1
HiagoAlonso <105528790+HiagoAlonso@users.noreply.github.com>
2022-06-11 20:03:24 -0300

    Initial commit

---
+++ 1st-release/game.js
@@ -0,0 +1,93 @@
+export default function createGame() {
+
+    const state = {
+        players: {},
+        fruits: {},
+        screen: {
+            width: 10,
+            height: 10
+        }
+    }
+
+    function addPlayer(command){
+        const playerId = command.playerId
+        const playerX = command.playerX
+        const playerY = command.playerY
+
+        state.players[playerId] = {
+            x: playerX,
+            y: playerY
+        }
+    }
+
+    function removePlayer(command){
+        const playerId = command.playerId
+        delete state.players[playerId]
+    }
+
+    function addFruit(command){
+        const fruitId = command.fruitId
+        const fruitX = command.fruitX
+        const fruitY = command.fruitY
+
+        state.fruits[fruitId] = {
+            x: fruitX,
+            y: fruitY
+        }
+    }
+    
+    function removeFruit(command){
+        const fruitId = command.fruitId
+        delete state.fruits[fruitId]
+    }
+
+    function movePlayer(command) {
+        const acceptedMoves = {
+            ArrowUp(player) {
+                player.y = Math.max(player.y - 1, 0);
+            },
+            ArrowRight(player) {
+                player.x = Math.min(player.x + 1, state.screen.width - 1);
+            },
+            ArrowDown(player) {
+                player.y = Math.min(player.y + 1, state.screen.height - 1);
+            },
+            ArrowLeft(player) {
+                player.x = Math.max(player.x - 1, 0);
+            }
+        }
+
+        const keyPressed = command.keyPressed
+        const playerId = command.playerId
+        const player = state.players[command.playerId]
+        const moveFunction = acceptedMoves[keyPressed]
+
+        if (player && moveFunction){
+            moveFunction(player)
+            checkForFruitsCollision(playerId)
+        }
+    }
+
+    function checkForFruitsCollision(playerId) {
+        const player = state.players[playerId]
+
+        for (const fruitId in state.fruits) {
+            const fruit = state.fruits[fruitId]
+            console.log(`Checking for ${playerId} and ${fruitId}`)
+
+            if (player.x === fruit.x && player.y == fruit.y) {
+                console.log(`COLLISION! between ${playerId} and ${fruitId}`)
+                removeFruit({fruitId})
+            }
+        }
+    }
+
+    return {
+        movePlayer,
+        addPlayer,
+        removePlayer,
+        addFruit,
+        removeFruit,
+        state
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto-rects
+ Projeto de aprendizado de Arquitetura de Software com um Jogo Multiplayer em JS
